996986578cbf5d41c71c8136700312dfa36468f7
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2024-03-28 19:06:42 -0300

    fix: rename the class name

--- src/test/java/me/dio/sdw24/application/listChampionsUseCaseIntegrationTest.java
+++ src/test/java/me/dio/sdw24/application/listChampionsUseCaseIntegrationTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class ListChampioinsUseCaseIntegrationTest {
+public class listChampionsUseCaseIntegrationTest {
 
     @Autowired
     private ListChampionsUseCase listChampioinsUseCase;
